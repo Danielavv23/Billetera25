44bdc6a4b1dac72d68ac8d20d2cecb8324ec910d
Daniela <danielavera@seti.com.co>
2025-06-03 08:08:25 -0500

    pruebadani

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,11 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return "Hola Mundo";
+ //   return this.appService.getHello();
+  }
+  @Get("/daniela")
+  getHelloDaniela(): string {
+    return "Hola Daniela";
   }
 }
