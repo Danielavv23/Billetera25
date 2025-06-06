5f01807e46bde6864bff1e8e60fc4cbe129a7425
josealvarino <josealvarino@seti.com.co>
2025-06-06 07:43:24 -0500

    prueba jose

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,7 +12,7 @@ export class AppController {
   }
   @Get("/daniela")
   getHelloDaniela(): string {
-    return "Hola Daniela";
+    return "Hola Jose y Daniela";
   }
 @Get("/jose")
   getHello_jose(): string {
